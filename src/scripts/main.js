3f2179d788098e8d30bc6119d5033df477d14bbc
Maksym <zubchenko.2001@gmail.com>
2026-04-30 13:47:27 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+const list = document.querySelector('ul');
+
+list.addEventListener('click', (ev) => {
+  ev.preventDefault();
+
+  let link = ev.target.parentElement;
+
+  if (ev.target.tagName === 'IMG') {
+    link = ev.target.parentElement;
+  }
+
+  if (link.tagName !== 'A') {
+    return;
+  }
+
+  largeImg.src = link.href;
+});
