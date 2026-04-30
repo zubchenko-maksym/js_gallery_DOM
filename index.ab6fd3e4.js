e927ce9b11c50b5b013a0719eb9567f617ea2807
Maksym <zubchenko.2001@gmail.com>
2026-04-30 13:47:37 +0300

    make build

---
+++ index.ab6fd3e4.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__large-img");document.querySelector("ul").addEventListener("click",function(t){t.preventDefault();var r=t.target.parentElement;"IMG"===t.target.tagName&&(r=t.target.parentElement),"A"===r.tagName&&(e.src=r.href)});
+//# sourceMappingURL=index.ab6fd3e4.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const largeImg = document.querySelector(".gallery__large-img");
+const list = document.querySelector("ul");
+list.addEventListener("click", (ev)=>{
+    ev.preventDefault();
+    let link = ev.target.parentElement;
+    if (ev.target.tagName === "IMG") link = ev.target.parentElement;
+    if (link.tagName !== "A") return;
+    largeImg.src = link.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
